35c034accc952f91bb152e59bbab18c3b476eecc
Ahnming Loke <lokeahnming@gmail.com>
2020-06-28 17:15:31 -0400

    feat: set up directory architecture, base markup, styles + pseudocode for logic

---
+++ js/app.js
@@ -0,0 +1,13 @@
+console.log('app.js');
+
+// REST Service Class - Handles XHR requests
+  // use XHR to make request
+  // only GET method needed
+  // return promise for async handling
+
+// Scroll Service Class - Listens and Changes size of lightbox on Scroll
+  // listen for scroll Event
+  // onScroll handler - calculate new width of div
+
+// UI Class - Handles Lightbox UI
+  // Event: Display data from XHR Requests
