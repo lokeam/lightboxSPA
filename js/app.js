3d3cd93f1984224eee42cfabf1aa52e58a0b80f8
Ahnming Loke <lokeahnming@gmail.com>
2020-06-28 18:09:21 -0400

    feat: promisify xhr request into RESTService, decouple http GET method

--- js/app.js
+++ js/app.js
@@ -5,21 +5,51 @@ console.log('app.js');
   // only GET method needed
   // return promise for async handling
 
-const getData = () => {
-  const xhr = new XMLHttpRequest();
-
-  const ENDPOINT = 'https://jsonplaceholder.typicode.com/todos/1';
-
-  xhr.open('GET', ENDPOINT);
-
-  xhr.onload = () => {
-    const data = JSON.parse(xhr.response);
-    console.log(data);
+class RESTService {
+  constructor() {
+    this._rawResponse = null;
   }
 
-  xhr.send();
+  _makeRequest(resource = '', method = 'GET') {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, resource, true);
+
+    return new Promise(function(resolve, reject) {
+      xhr.responseType = 'json';
+
+      xhr.setRequestHeader('Content-Type', 'application/json');
+
+      xhr.onload = function(e) {
+        if (xhr.status > 300) {
+          reject('Error, status code: ', xhr.status);
+        } else {
+
+          // in case we need to save the raw response:
+          this._rawResponse = xhr.response
+          resolve(xhr.response);
+        }
+      }
+      xhr.send();
+    })
+  }
+
+  get(resource = '') {
+    return this._makeRequest(resource, 'GET');
+  }
 }
 
+async function getData() {
+  const ENDPOINT = 'http://homework.warbyparker.com';
+  const RESTClient = new RESTService();
+
+  try {
+    let response = await RESTClient.get(ENDPOINT);
+    console.log('testing response from async: ', response);
+  } catch(error) {
+    console.log('RESTService error. How are you gentlepeople !! All your base are belong to us: ', error);
+  }
+}
 getData();
 
 
