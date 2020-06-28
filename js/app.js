427bd5cf498eaeeb72c38e5739b03dd6ddeee20b
Ahnming Loke <lokeahnming@gmail.com>
2020-06-28 17:55:36 -0400

    feat: succesfully tested barebones xhr request

--- js/app.js
+++ js/app.js
@@ -5,9 +5,28 @@ console.log('app.js');
   // only GET method needed
   // return promise for async handling
 
+const getData = () => {
+  const xhr = new XMLHttpRequest();
+
+  const ENDPOINT = 'https://jsonplaceholder.typicode.com/todos/1';
+
+  xhr.open('GET', ENDPOINT);
+
+  xhr.onload = () => {
+    const data = JSON.parse(xhr.response);
+    console.log(data);
+  }
+
+  xhr.send();
+}
+
+getData();
+
+
 // Scroll Service Class - Listens and Changes size of lightbox on Scroll
   // listen for scroll Event
   // onScroll handler - calculate new width of div
 
 // UI Class - Handles Lightbox UI
   // Event: Display data from XHR Requests
+
